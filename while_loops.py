34f9beb49b923d850838ef5a9342cb8470bc2fde
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-10-15 13:29:53 +0700

    updates

---
+++ while_loops.py
@@ -0,0 +1,8 @@
+num = 0
+
+while num <= 100:
+    print(num)
+    num = num + 1
+
+#while berguna ketika ingin mencapai hal yan kita ingin kan, ketika
+#object tidak sesuai dengan angka yang sebenarnya.
