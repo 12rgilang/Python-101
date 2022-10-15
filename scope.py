5c605a033eef2c2965f7d3a77aab185baac067b8
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-10-15 14:33:10 +0700

    Updates

---
+++ scope.py
@@ -0,0 +1,10 @@
+name = "Gilang"
+
+def myfunc(name):
+    # name = "New name" #scope
+    return name         #scope
+                        #scope
+print(myfunc())
+print(name)
+
+#scope is code inside a function
